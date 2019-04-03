a3d3288e95f0b2ffcd72164e006b07efe578e4c8
Greg Roques <greg.roques@gmail.com>
2019-04-03 09:22:28 -0400

    adding routes

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,22 +1,32 @@
 import React, {Component} from "react";
-// import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link}  from 'react-router-dom';
+
 import "./Home.css"
+
+import About from '../About/About'
+import Blog from '../Blog/Blog'
+import Portfolio from '../../Components/Portfolio/Portfolio'
 
 class Home extends Component{
 
     render(){
         return(
-            <div className="homeBody">
-                <div className="padding768 circlesJustify">
-                    <div className="myStory"><span>About</span></div>
+        <Router>
+                <div className="homeBody">
+                    <div className="padding768 circlesJustify">
+                    <Link style={{ textDecoration: 'none' }} to="about"><div className="myStory"><span>About</span></div></Link> 
+                    </div>
+                    <div className="padding768 circlesJustify">
+                        <Link style={{ textDecoration: 'none' }} to="portfolio"><div className="projects">Portfolio</div></Link>
+                    </div>
+                    <div className="circlesJustify">
+                        <Link style={{ textDecoration: 'none' }} to="blog"><div className="blog">Blog</div></Link>
+                    </div>                
                 </div>
-                <div className="padding768 circlesJustify">
-                    <div className="projects">Portfolio</div>
-                </div>
-                <div className="circlesJustify">
-                    <div className="blog">Blog</div>
-                </div>                
-        </div>
+                <Route path="/about" component={About}/>
+                <Route path="/portfolio" component={Portfolio}/>
+                <Route path="/blog" component={Blog}/>
+            </Router>
         )
     }
 
