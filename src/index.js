49775ee63a7cccb4a53b1a32ec7fa4485a17ef13
Greg Roques <greg.roques@gmail.com>
2019-07-01 12:07:33 -0400

    more analytics

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ import reducers from "./Reducers/RootReducer.js";
 import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 import { trackingId } from './AxiosOrders'
+import { get } from 'http';
 
 const history = createBrowserHistory();
 ReactGA.initialize(trackingId);
 history.listen(location => {
-    ReactGA.set({ page: location.pathname }); 
+    ReactGA.set({ 
+        page: location.pathname
+    }); 
     ReactGA.pageview(location.pathname); 
   });
 
