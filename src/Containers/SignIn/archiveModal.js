b6d846170c770321ac043e8ae4744cc27798f9ce
Greg Roques <greg.roques@gmail.com>
2019-08-17 12:00:21 -0400

    optimized modal render

--- src/Containers/SignIn/archiveModal.js
+++ src/Containers/SignIn/archiveModal.js
@@ -7,25 +7,24 @@ const ArchiveModal = props => {
         update.Modal,
         props.show ? update.ModalOpen : update.ModalClosed
     ];
-
-  return (
-    <div className={cssModal.join(' ')}>
-        <h1 className={update.archiveTitle}>Archives</h1>
-        {Object.keys(props.articles).reverse().map((post,i)=> {
-            return(
-                <div key={i}>
-                    <span className={update.oldArticle} onClick={()=>props.updateArticle(post)}>
-                        {props.articles[post].date}
-                    </span>
-                    <span className={update.deleteButton} onClick={()=>props.existingDelete(post)}>DELETE</span>
-                </div>
-            )
-        })}
-        <button className={update.closeButton} onClick={props.closed}>
-            Close
-        </button>
-    </div>
-  );
+    return props.show ? (
+            <div className={cssModal.join(' ')}>
+                <h1 className={update.archiveTitle}>Archives</h1>
+                {Object.keys(props.articles).reverse().map((post,i)=> {
+                    return(
+                        <div key={i}>
+                            <span className={update.oldArticle} onClick={()=>props.updateArticle(post)}>
+                                {props.articles[post].date}
+                            </span>
+                            <span className={update.deleteButton} onClick={()=>props.existingDelete(post)}>DELETE</span>
+                        </div>
+                    )
+                })}
+                <button className={update.closeButton} onClick={props.closed}>
+                    Close
+                </button>
+            </div>
+        ) : null;
 };
 
 export default ArchiveModal;
