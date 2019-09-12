83926470180af59b882483e0424dfe6c258b6f3f
Greg Roques <greg.roques@gmail.com>
2019-09-12 14:21:35 -0400

    final update

--- src/Containers/Design/Design.js
+++ src/Containers/Design/Design.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Photography from './Photography'
 import Magazines from './Magazines'
 import Articles from './Articles'
+import Film from './Film'
 import "./Design.css";
 
 // Redux
@@ -23,6 +24,7 @@ class Design extends Component{
                 <Magazines/>
                 <Photography/>
                 <Articles/>
+                <Film />
             </div>
         )
     }
