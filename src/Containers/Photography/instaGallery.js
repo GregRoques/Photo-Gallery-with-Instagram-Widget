0642639e7c02b1544441399d683918c5a8adcca8
Greg Roques <greg.roques@gmail.com>
2020-02-22 12:17:43 -0500

    update

---
+++ src/Containers/Photography/instaGallery.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import css from './instaGallery.module.css';
+
+class instaGallery extends Component {
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+
+        );
+    };
+}
+
+export default instaGallery;
