fb8645446b5f414d86b91f7bc1cc58f209ecc32a
Greg Roques <greg.roques@gmail.com>
2020-03-01 14:11:39 -0500

    update

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+
+
+app.use(cors());
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(2000);
+console.log('App is runung on port 2000');
