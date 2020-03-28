eb69d743f9a9469eeaf671536b0267988320fd9c
Greg Roques <greg.roques@gmail.com>
2020-03-28 09:55:24 -0400

    plaket

--- src/Containers/Photography/instaGallery.js
+++ src/Containers/Photography/instaGallery.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import instaCss from './instaGallery.module.css';
 
-const pic = "https://i.etsystatic.com/10283697/r/il/fd147b/1014181977/il_570xN.1014181977_5sx7.jpg";
-
 class instaGallery extends Component {
     state={
         profilePic: "https://lh3.googleusercontent.com/proxy/qRSmXP4xklfeIxWJx47NWPPOXTw98JUMwIVIoxmG_TWApKRltySXzMc-ylnZmeKHQudlxamstWDBhuFE3iAGJTDBl3pBsgncI9zfj7-eEidHxiOJTXN79WZb4rrUA-tVLBnqH_kqZOEHiQ",
-        images: [
+        image: [
             { 
                 pic: "https://cdn.mos.cms.futurecdn.net/vChK6pTy3vN3KbYZ7UU7k3-1200-80.jpg",
                 date: "March 26, 2020",
@@ -34,43 +32,55 @@ class instaGallery extends Component {
             }
 
         ],
+        selectedPic: {
+            pic: "",
+            date: "",
+            caption: "",
+            index: ""
+        },
         display: false
     }
 
-    instaPopUp = (pic, date, caption, index) =>{
-        this.isPopUpOpen()
-        return(
+    instaPopUp = () =>{
+        const { selectedPic } = this.state;
+        return  this.state.display === true ? (
             <div className={instaCss.centerAndBackground}>
-                <div onClick={() => this.isPopUpOpen()} className={instaCss.closeButton}>X</div>
+                <div onClick={() => this.isPopUpOpen( "", "", "", "")} className={instaCss.closeButton}>X</div>
                 <div className={instaCss.selectedContainer}>
                     <div className={instaCss.selectedHeader}>
-                        <img alt={ pic} className={instaCss.selectedHeaderImage} src={ this.state.profilePic }/>
+                        <img alt={ selectedPic.pic} className={instaCss.selectedHeaderImage} src={ this.state.profilePic }/>
                         <a className={instaCss.selectedHyperlink} href="https://www.instagram.com/qtrmileatatime" target="_blank" rel="noopener noreferrer nofollow">qtrmileatatime</a> 
                     </div>
                     <div className={instaCss.selectedImageContainer}>
-                        <img alt={ index } src={ pic }/>
+                        <img class={instaCss.chosenImage} alt={ selectedPic.index } src={ selectedPic.pic }/>
                     </div>
                     <div className={instaCss.selectedCaption}>
-                        <b>{ date }</b><br/>
-                        { caption }
+                        <b>{ selectedPic.date }</b><br/>
+                        { selectedPic.caption }
                     </div>
                 </div>
             </div>
-        )
+        ) : null
     }
 
-    isPopUpOpen = () =>{
+    isPopUpOpen = (newPic, newDate, newCaption, newIndex) =>{
         const { display } = this.state;
         this.setState({
-            display: !display
+            display: !display,
+            selectedPic: {
+                pic: newPic,
+                date: newDate,
+                caption: newCaption,
+                index: newIndex
+            },
         })
     }
 
     render(){
         const { image } = this.state;
-        return this.state.images.length === 5 ? (
+        return this.state.image.length === 5 ? (
             <div className={instaCss.instaModuleSpacing}>
-                { this.state.display === true ? <this.instaPopUp/> : null }
+                <this.instaPopUp/> 
                  <div className={instaCss.container}>
         <div className={instaCss.header}>
             <a href="https://www.instagram.com/qtrmileatatime" target="_blank" rel="noopener noreferrer nofollow">
@@ -79,17 +89,17 @@ class instaGallery extends Component {
         </div>
         <div className={instaCss.hitemwiththatflexRow}>
             <div className={instaCss.hitemwiththatflexColumn1}>
-                <img className={instaCss.bigPicture} alt={ "insta1" } onClick={()=> this.instaPopUp(image[0].pic, image[0].date, image[0].caption, "insta1")} src={ image[0].pic }/>
+                <img className={instaCss.bigPicture} alt={ "insta1" } onClick={()=> this.isPopUpOpen(image[0].pic, image[0].date, image[0].caption, "insta1")} src={ image[0].pic }/>
             </div>
             
             <div className={instaCss.hitemwiththatflexColumn2}>
-                <img className={instaCss.smallPicture1} alt={ "insta2" } onClick={()=> this.instaPopUp(image[1].pic, image[1].date, image[1].caption, "insta2")} src={ image[1].pic }/>
-                <img className={instaCss.smallPicture2} alt={ "insta3"  } onClick={()=> this.instaPopUp(image[2].pic, image[2].date, image[2].caption, "insta3")} src={ image[2].pic }/>
+                <img className={instaCss.smallPicture1} alt={ "insta2" } onClick={()=> this.isPopUpOpen(image[1].pic, image[1].date, image[1].caption, "insta2")} src={ image[1].pic }/>
+                <img className={instaCss.smallPicture2} alt={ "insta3"  } onClick={()=> this.isPopUpOpen(image[2].pic, image[2].date, image[2].caption, "insta3")} src={ image[2].pic }/>
             </div>
             
             <div className={instaCss.hitemwiththatflexColumn3}>
-                <img className={instaCss.smallPicture1} alt={ "insta4" } onClick={()=> this.instaPopUp(image[3].pic, image[3].date, image[3].caption, "insta4")} src={ image[3].pic }/>
-                <img className={instaCss.smallPicture2} alt={ "insta5" } onClick={()=> this.instaPopUp(image[4].pic, image[4].date, image[4].caption, "insta4")} src={ image[4].pic }/>
+                <img className={instaCss.smallPicture1} alt={ "insta4" } onClick={()=> this.isPopUpOpen(image[3].pic, image[3].date, image[3].caption, "insta4")} src={ image[3].pic }/>
+                <img className={instaCss.smallPicture2} alt={ "insta5" } onClick={()=> this.isPopUpOpen(image[4].pic, image[4].date, image[4].caption, "insta4")} src={ image[4].pic }/>
             </div>
         </div>
     </div>
