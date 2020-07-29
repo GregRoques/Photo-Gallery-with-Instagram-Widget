289b0921dc187ed77a90a03ca2a26f4caeddc775
Greg Roques <greg.roques@gmail.com>
2020-07-29 17:11:57 -0400

    update

--- src/App.js
+++ src/App.js
@@ -59,7 +59,12 @@ class App extends Component {
               {/* Backend Update Blog */}
                 <Route exact path = '/user-update-blog' component={Update}/>
               {/* Re-Route Non-Existant Pages */}
+                <Route path='/resume' component={() => { 
+                    window.location.href = "https://www.gregroques.com/images/socialIcons/Resume.pdf"; 
+                    return null;
+                }}/>
                 <Route component ={this.NoPage}/>
+               
            </Switch>
         </Layout>
       </div>
