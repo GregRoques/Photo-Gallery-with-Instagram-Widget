00157a6701b8cab21307c445f4a40b349f384f3b
Greg Roques <greg.roques@gmail.com>
2020-11-04 12:19:54 -0500

    deprecated Insta

--- Portfolio–User/src/Containers/HeadFoot/Footer.js
+++ Portfolio–User/src/Containers/HeadFoot/Footer.js
@@ -1,60 +1,8 @@
 import React, {Component} from "react";
-import { Link } from 'react-router-dom';
 import "./HeadFoot.css";
 import ResumeModal from './ResumeModal';
+import { Footer, LinkedIn, GitHub, Resume} from './FooterLinks'
 
-const Contact = ({ isEmailToggled, isShown  }) => {
-    return(
-        <div className="contact socialIcon" onClick={isEmailToggled}>
-            <img className="contact1"  src= '/images/socialIcons/contact2.jpg' alt='greg@gregroques.com'/> 
-            { !isShown ?  <img className="contact2" src= '/images/socialIcons/contact1.jpg' alt='greg@gregroques.com'/>: null}
-        </div>
-    )
-}
-
-const LinkedIn = () => {
-    return(
-        <div className="li socialIcon">
-            <a href='https://www.linkedin.com/in/gregroques/' rel="noopener noreferrer" target="_blank">
-                <img className="li1"  src= '/images/socialIcons/li2.jpg' alt='LinkedIn: @GregRoques'/>
-                <img className="li2" src= '/images/socialIcons/li1.jpg' alt='LinkedIn: @GregRoques'/>
-            </a>
-        </div>
-    )
-};
-
-const GitHub = () => {
-    return(
-        <div className="gh socialIcon">
-            <a rel="noopener noreferrer" target="_blank" href='https://github.com/GregRoques' >
-                <img className="gh1"  src= '/images/socialIcons/gh2.jpg' alt='GitHub: @GregRoques'/>
-                <img className="gh2" src= '/images/socialIcons/gh1.jpg' alt='GitHub: @GregRoques'/>
-            </a>
-        </div>
-    )
-};
-
-const Resume = () => {
-    return(
-        <div className="resume socialIcon">
-            <Link onClick={e =>{ e.preventDefault(); window.open("https://www.gregroques.com/images/socialIcons/Resume.pdf")}}>
-                <img className="resume1"  src= '/images/socialIcons/pdf2.jpg' alt='2019 Resume'/>
-                <img className="resume2" src= '/images/socialIcons/pdf1.jpg' alt='2019 Resume'/>
-            </Link>
-        </div>
-    )
-};
-
-const Insta = () => {
-    return(
-        <div className="insta socialIcon">
-            <a href='https://www.instagram.com/qtrmileatatime' rel="noopener noreferrer" target="_blank">
-                <img className="insta1"  src= '/images/socialIcons/insta2.jpg' alt='Insta: @qtrmileatatime'/>
-                <img className="insta2" src= '/images/socialIcons/insta1.jpg' alt='Insta: @qtrmileatatime'/>
-            </a>
-        </div>
-    )
-}
 
 class Footer extends Component{
 
@@ -93,7 +41,7 @@ class Footer extends Component{
                             <GitHub/>
                             <Resume/>
                         </> :
-                            <Insta/>
+                            ""
                     }
                 </div>
             </div>
